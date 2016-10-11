810f76386ef4e603448b3d5521842408cfce4a14
Alicia Miceli <aliciamiceli@Alicias-MacBook-Pro.local>
2016-10-10 19:17:37 -0500

    working with promises

--- javascripts/main.js
+++ javascripts/main.js
@@ -4,24 +4,47 @@ $(document).ready(function() {
   console.log("jquery is ready");
   var contentEl = $("#all-my-songs");
   var songs = [];
-  $.ajax({
-    url: "../songs.json"
-  }).done(function(data) {
-    songs = data.songs;
-    console.log("data", data);
-    $.ajax({
-      url: "../songs2.json"
+
+  function getSongs(){
+    return new Promise((resolve, reject) => {
+      $.ajax({
+        url: "../songs.json"
+      }).done(function(data) {
+        resolve(data);
+      }).fail(function(xhr, status, error){
+        reject(error);
+      });
+    });
+  }
+
+  function getSongs2(resultOfFirstAjax){
+    return new Promise((resolve, reject) => {
+      $.ajax({
+        url: "../songs2.json"
       }).done(function(data2) {
-        console.log("data2", data2);
+        songs = resultOfFirstAjax.songs;
+        resolve(data2);
+      }).fail(function(xhr2, status2, error2){
+        reject(error2);
+      });
+    });
+  }
+
+getSongs().then(function(dataPass){
+  console.log("dataPass", dataPass);
+  console.log("song 1st then", songs); //empty array
+  return getSongs2(dataPass);
+}).then(function(dataPass2){
+  console.log("song second then", songs); //three songs from first ajax
+  console.log("dataPass2", dataPass2); //three songs inside an array - second json object
         var songData = "";
         var currentSong;
-        
-        data2.songs.forEach(function(song){
+      dataPass2.songs.forEach(function(song){
           songs.push(song);
         });
 
         console.log("songs", songs);
-        
+
         for (var i = 0; i < songs.length; i++) {
           currentSong = songs[i];
 
@@ -38,10 +61,51 @@ $(document).ready(function() {
 
         console.log("songData", songData);
         contentEl.html(songData);
-      }).fail(function(error) {
-        console.log( "error" , error);
-      });
-  }).fail(function(error) {
-    console.log( "error" , error);
-  });
+
+});
+// getSongs2().then(function(dataPass2){
+//   console.log("dataPass2", dataPass2);
+// });
+
+
+  // $.ajax({
+  //   url: "../songs.json"
+  // }).done(function(data) {
+  //   songs = data.songs;
+  //   console.log("data", data);
+  //   $.ajax({
+  //     url: "../songs2.json"
+  //     }).done(function(data2) {
+  //       console.log("data2", data2);
+  //       var songData = "";
+  //       var currentSong;
+
+  //       data2.songs.forEach(function(song){
+  //         songs.push(song);
+  //       });
+
+  //       console.log("songs", songs);
+
+  //       for (var i = 0; i < songs.length; i++) {
+  //         currentSong = songs[i];
+
+  //         songData += "<div class='song-block'>";
+  //           songData += "<h1>" + currentSong.title + "</h1>";
+  //           songData += "<div class='artist'>Performed by ";
+  //             songData += currentSong.artist;
+  //           songData += "</div>";
+  //           songData += "<div class='album'>On the album ";
+  //             songData += currentSong.album;
+  //           songData += "</div>";
+  //         songData += "</div>";
+  //       }
+
+  //       console.log("songData", songData);
+  //       contentEl.html(songData);
+  //     }).fail(function(error) {
+  //       console.log( "error" , error);
+  //     });
+  // }).fail(function(error) {
+  //   console.log( "error" , error);
+  // });
 });
